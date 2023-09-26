ba0b7d165c14ccac6ce1dcd186115c488e843dc6
svetlanael12 <svetlana_el12@mail.ru>
2023-09-26 08:36:58 +0500

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGO_CONNECTION),
+        TodoModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
